41c55683ca798c547caa73e582eedcfaeef56b61
--add <adeelahmad2295@gmail.com>
2024-09-03 17:24:47 +0530

    isAdult

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const age = 22;
-const name = 'Adeel';
-const email = 'xyz123@gmail.com';
+const name = "Adeel";
+const email = "xyz123@gmail.com";
 let roll_no = 32;
-const dob = '25/05/2001';
+const dob = "25/05/2001";
+const a = 23;
+const b = 34;
+console.log(a * b);
 
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function myfunk() {
-  console.log("this is a function");
+const dob = '25/05/2024'
+const isAdult = () => {
+  if (isAdult < 18) {
+    console.log("notAdult");
+  }
 }
